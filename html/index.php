a53b6ad93f7db1fa0f49bd7054a7ecd281f083ae
Futa Hirakoba <korosuke613613@gmail.com>
2017-02-02 04:52:05 +0900

    Create Standard files

---
+++ html/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<!-- 共通<head>タグ -->
+<?php include (dirname(__FILE__).'/parts/head_tag.html'); ?>
+
+
+<head>
+    <title>片山(徹)研究室 : 宮崎大学</title>
+</head>
+
+
+<body>
+
+    <!-- ヘッダー -->
+    <?php include (dirname(__FILE__).'/parts/header.html'); ?>
+
+    <!-- メニュー -->
+    <?php include (dirname(__FILE__).'/parts/menu.html'); ?>
+
+    <!-- 本文ブロック -->
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12" style="background:#999;height: 100vh;">
+                <h2>本文ブロック</h2>
+                <div id="sssbarName">ああああああああ</div>
+            </div>
+        </div>
+    </div>
+
+    <!-- フッター -->
+    <?php include (dirname(__FILE__).'/parts/footer.html'); ?>
+
+    <!-- Javascriptの読み込み -->
+    <?php include (dirname(__FILE__).'/parts/js_load.html'); ?>
+    
+</body>
+
+</html>
