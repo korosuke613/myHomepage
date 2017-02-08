536e0ed6bd32529b6f6670ee2eeb15b13f743386
Futa Hirakoba <korosuke613613@gmail.com>
2017-02-09 02:25:57 +0900

    Finish smooth page jump

--- js/page_scroll.js
+++ js/page_scroll.js
@@ -1,10 +1,12 @@
-$(function(){
-    $('a[href^=#]').click(function(){
+$(function () {
+    $('a[href^="#"]').click(function () {
         var speed = 500;
-        var href= $(this).attr("href");
+        var href = $(this).attr("href");
         var target = $(href == "#" || href == "" ? 'html' : href);
         var position = target.offset().top;
-        $("html, body").animate({scrollTop:position}, speed, "swing");
+        $("html, body").animate({
+            scrollTop: position
+        }, "slow", "swing");
         return false;
     });
 });
