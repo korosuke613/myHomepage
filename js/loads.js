923b342bd152901267ff6e942b2fb768cb7c8495
Futa Hirakoba <korosuke613613@gmail.com>
2017-03-13 02:36:55 +0900

    Modify catch async

--- js/loads.js
+++ js/loads.js
@@ -2,8 +2,8 @@ function load_html(pathHTML) {
 
     $.ajax({
         url: pathHTML, //パスはcommon.jsが読み込まれたHTMLファイルが基準になります
-        cache: false, //キャッシュを利用するか（お好みで）
-        async: false, //非同期で読み込むか（お好みで）
+        cache: true, //キャッシュを利用するか（お好みで）
+        async: true, //非同期で読み込むか（お好みで）
         success: function (html) {
             html = html.replace(pathHTML);
             document.write(html);
