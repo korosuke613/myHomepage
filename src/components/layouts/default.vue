bf8f33f49dd6d688338ddcf9d16c23f77baedc83
Futa Hirakoba <korosuke613613@gmail.com>
2019-05-03 23:08:57 +0900

    add default layout

---
+++ src/components/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header/>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "../molcules/header.vue";
+import Footer from "../molcules/footer.vue"
+export default {
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
---
+++ src/components/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>ここはBeeenoの情報をまとめるwikiです。</div>
+</template>
+
+<script>
+export default {
+  layout: "default",
+  head() {
+    return {
+      title: "Korosuke613@miyazaki",
+      titleTemplate: ""
+    };
+  }
+};
+</script>
