8303713c40c6fa1f6de1d2cb33da18322259280d
Futa Hirakoba <korosuke613613@gmail.com>
2019-05-04 03:38:06 +0900

    Activityを分割

--- client/components/Activity.vue
+++ client/components/Activity.vue
@@ -1,57 +1,22 @@
 <template>
   <div>
     <h1>Activity</h1>
-    <a
-      href="https://github.com/korosuke613/miyadai-sso-auto-login"
-      target="_blank"
-      onclick="ga('send', 'event', 'link', 'make', 'miyadai_auto_login')"
-      class="make"
-    >
-      <span style="color:#000">2018/08</span>宮大SSO自動ログインスクリプト
-    </a>
-    <br>
-    <a
-      href="https://github.com/korosuke613/pict-java"
-      target="_blank"
-      onclick="ga('send', 'event', 'link', 'make', 'pict4java')"
-      class="make"
-    >
-      <span style="color:#000">2018/07</span>PICTをJava/Kotlinから利用するJNAクラス
-    </a>
-    <br>
-    <a
-      href="https://hub.docker.com/r/korosuke613/etrobo-docker/"
-      target="_blank"
-      onclick="ga('send', 'event', 'link', 'make', 'etrobo_docker')"
-      class="make"
-    >
-      <span style="color:#000">2017/12</span>ETロボコンのビルド環境Docker Image
-    </a>
-    <br>
-    <a
-      href="http://earth.cs.miyazaki-u.ac.jp/seminar/"
-      target="_blank"
-      onclick="ga('send', 'event', 'link', 'make', 'senmon2017')"
-      class="make"
-    >
-      <span style="color:#000">2017/10</span>専門教育入門セミナーT 2017 in 片山（徹）研
-    </a>
-    <br>
-    <a
-      href="https://github.com/korosuke613/etrobocon2017"
-      target="_blank"
-      onClick="ga('send', 'event', 'link', 'make', 'ETrobocon2017')"
-      class="make"
-    >
-      <span style="color:#000">2017/09</span>ETロボコン2017
-    </a>
-    <a
-      href="https://twitter.com/miya_330_bot"
-      target="_blank"
-      onClick="ga('send', 'event', 'link', 'make', 'MiyadaiOshiraseBot')"
-      class="make"
-    >
-      <span style="color:#000">2017/07</span>宮大支援課お知らせBOT【非公式】
-    </a>
+    <div v-for="item in items" :key="item.href">
+      <a :href="item.href" target="_blank" :onClick="item.ga" class="make">
+        <span style="color:#000">{{item.day}}</span>
+        {{item.title}}
+      </a>
+    </div>
   </div>
 </template>
+
+<script>
+import {items} from "@/assets/json/ActivityData.js";
+export default {
+  data() {
+    return {
+      items: items.slice(0, 6)
+    };
+  }
+};
+</script>
