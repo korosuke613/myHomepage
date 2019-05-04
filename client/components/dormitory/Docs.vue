5936ff274adebbeb67c59f4b1899250be414f207
Futa <korosuke613613@gmail.com>
2019-05-04 15:49:02 +0900

    refactoring dormitory

---
+++ client/components/dormitory/Docs.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>
+      Docs
+      <small>資料</small>
+    </h2>僕が寮長だった頃に作成した資料の一部です。寮役に関する資料は寮役を目指す1年生のために公開しています。参考にしてください。
+    <br>
+    <br>
+    <a
+      href="./sources/narou_ryouyaku.pdf"
+      onClick="ga('send', 'event', 'download', 'pdf', 'narou_ryouyaku.pdf')"
+    >
+      <span style="color:#000">2016/10</span> 寮役紹介資料
+    </a>
+    <br>
+    <a
+      href="./sources/ryouyaku.pdf"
+      onClick="ga('send', 'event', 'download', 'pdf', 'ryouyaku.pdf')"
+    >
+      <span style="color:#000">2016/10</span> 宮崎大学国際交流宿舎における寮役制度
+    </a>
+    <br>
+    <a
+      href="./sources/kouhosei_yotei.pdf"
+      onClick="ga('send', 'event', 'download', 'pdf', 'kouhosei_yotei.pdf')"
+    >
+      <span style="color:#000">2016/10</span> 寮役候補生予定表
+    </a>
+    <br>
+    <a
+      href="./sources/bihin_shinsei.pdf"
+      onClick="ga('send', 'event', 'download', 'pdf', 'bihin_shinsei.pdf')"
+    >
+      <span style="color:#000">2016/11</span> 備品修理申請書
+    </a>
+    <br>
+  </div>
+</template>
+
---
+++ client/components/dormitory/Notice.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="hidden-xxs col-xs-12 col-sm-5 col-md-4">
+      <h2>
+        Notice
+        <small>イベントお知らせ</small>
+      </h2>
+      <a
+        class="twitter-timeline"
+        height="360px"
+        width="350px"
+        data-theme="dark"
+        data-chrome="”noheader"
+        transparent”
+        href="https://twitter.com/kokuryo_miya"
+      >Tweets by kokuryo_miya</a>
+    </div>
+    <div class="visible-xxs col-xxs-12">
+      <h2>
+        Notice
+        <small>イベントお知らせ</small>
+      </h2>
+      <div style="text-align:center;">
+        <a
+          class="twitter-timeline"
+          height="360px"
+          width="300px"
+          data-theme="dark"
+          data-chrome="”noheader"
+          transparent”
+          href="https://twitter.com/kokuryo_miya"
+        >Tweets by kokuryo_miya</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      script: [
+        {
+          src: "https://platform.twitter.com/widgets.js",
+          charset: "utf-8",
+          defer: true
+        }
+      ]
+    };
+  }
+};
+</script>
