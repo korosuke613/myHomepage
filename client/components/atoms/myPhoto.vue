9ec968982ba19d3a22e50ac069af310ac47921e8
Futa <korosuke613613@gmail.com>
2019-05-09 23:49:45 +0900

    fix

--- client/components/atoms/myPhoto.vue
+++ client/components/atoms/myPhoto.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <picture >
-      <source :src="require('@/assets/images/myPhoto.jpg?webp')" type="image/webp">
+      <source :srcset="require('@/assets/images/myPhoto.jpg?webp')" type="image/webp">
       <img class="img-responsive img-rounded" :src="require('@/assets/images/myPhoto.jpg')" alt="ひらこばの写真">
     </picture>
   </div>
