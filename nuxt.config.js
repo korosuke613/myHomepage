0d2b2599f655cd8b4aebeab50a4ac0ba2a6b4271
Futa <korosuke613613@gmail.com>
2019-05-09 22:33:29 +0900

    delete imagemin

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,12 +32,6 @@ export default {
         '@nuxtjs/google-analytics',
         '@nuxtjs/sitemap',
         '@nuxtjs/pwa',
-        ["nuxt-imagemin",
-            {
-                optipng: { optimizationLevel: 5 },
-                gifsicle: { optimizationLevel: 2 }
-            }
-        ],
     ],
     sitemap: {
         hostname: 'https://korosuke613.github.io',
