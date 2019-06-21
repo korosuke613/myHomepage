71afcd7555683bf3cd6ccb7e976e74eeff872b11
Futa Hirakoba <korosuke613613@gmail.com>
2019-06-21 16:22:49 +0900

    update Activity

--- client/assets/json/ActivityData.js
+++ client/assets/json/ActivityData.js
@@ -4,6 +4,42 @@ const genarateGA = (day) => {
 
 export const items = [
     {
+        href: "https://github.com/korosuke613/book-info-from-amazon",
+        ga: genarateGA('BIFA'),
+        day: "2019/03",
+        title: "amazonの本の情報を簡単に取得するChrome拡張"
+    },
+    {
+        href: "https://engineers.recruit-jinji.jp/event/s-internship-2019-engineer/",
+        ga: genarateGA('intern_recruit'),
+        day: "2019/03",
+        title: "リクルートライフスタイルインターン(1ヶ月)"
+    },
+    {
+        href: "https://www.researchgate.net/publication/331335335_Application_of_Pairwise_Testing_to_Test_Cases_by_Boundary_Value_Analysis_in_BWDM",
+        ga: genarateGA('icarob2019'),
+        day: "2019/01",
+        title: "ICAROB2019(査読有り) 論文発表"
+    },
+    {
+        href: "http://teams.etrobo.jp/2018/taikai/championsip_result2.php",
+        ga: genarateGA('et2018_cs'),
+        day: "2018/11",
+        title: "ETロボコン2018 全国大会出場(14チーム中8位)"
+    },
+    {
+        href: "http://kyushuminami.etrobo.jp/",
+        ga: genarateGA('et2018_ls'),
+        day: "2018/09",
+        title: "ETロボコン2018 九州南大会(DA優勝)"
+    },
+    {
+        href: "https://w2.gakkai-web.net/gakkai/jceee_pro/program/index.html",
+        ga: genarateGA('jceee2018'),
+        day: "2018/09",
+        title: "JCEEE2018 論文発表"
+    },
+    {
         href: "https://github.com/korosuke613/miyadai-sso-auto-login",
         ga: genarateGA('miyadai_auto_login'),
         day: "2018/08",
