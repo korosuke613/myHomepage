983a84315e6a5881e8b715d281bed6515e1da2c8
Futa Hirakoba <korosuke613613@gmail.com>
2019-06-27 19:38:21 +0900

    コメントを追加 [skip ci]

--- .circleci/generate_hatena.js
+++ .circleci/generate_hatena.js
@@ -1,64 +1,79 @@
-'use strict'
+"use strict";
 
-const axios = require('axios');
-const xml2js = require('xml2js');
-const moment = require('moment');
-const fs = require('fs');
-const HatenaItem = require('./HatenaItem');
+const axios = require("axios");
+const xml2js = require("xml2js");
+const moment = require("moment");
+const fs = require("fs");
+const HatenaItem = require("./HatenaItem");
 
-const get_xml =
-    async (url) => {
+// APIを叩いてはてなブログのXMLを取得する。
+const getHatenaData = async url => {
   try {
-    const res = await axios.get(
-        url, {auth: {username: 'korosuke613', password: process.env.HATENA_PASS}});
+    const res = await axios.get(url, {
+      auth: { username: "korosuke613", password: process.env.HATENA_PASS }
+    });
     return res.data;
   } catch (err) {
-    const {status, statusText} = err.response;
+    const { status, statusText } = err.response;
     console.log(`Error! HTTP Status: ${status} ${statusText}`);
   }
-}
+};
 
-const getItems =
-    (entry, item_list) => {
-      for (let e of entry) {
-        if (e['app:control'][0]['app:draft'][0] == 'yes') {
-          continue;
-        }
-        const item = new HatenaItem(
-            moment(e.published.toString()).format('YYYY-MM-DD'),
-            e.title.toString(), e.link[1].$.href);
-        // console.log(`${moment(e.published.toString()).format('YYYY-MM-DD')}${e.title.toString()}`)
-        item_list.push(item);
-      }
-    }
-
-const createItems =
-    async (data, item_list) => {
+// XMLから記事一覧と次の記事一覧のURIを抽出する。
+const extractItemsAndNextUri = async data => {
   return new Promise((resolve, reject) => {
     xml2js.parseString(data.toString(), (err, result) => {
       if (err) {
-        console.log(err);
+        reject(err);
       } else {
-        const entry = result.feed.entry;  // 配列になってる
+        const entry = result.feed.entry;
         const next_url = result.feed.link[1].$.href;
-        getItems(entry, item_list)
-        resolve(next_url);
+        resolve({ entry, next_url });
       }
     });
   });
-}
+};
 
-const main =
-    async () => {
-  let item_list = [];
-  let url =
-      'https://blog.hatena.ne.jp/korosuke613/korosuke613.hatenablog.com/atom/entry';
-  while (item_list.length < 10) {
-    const data = await get_xml(url);
-    url = await createItems(data, item_list);
+// 下書きを除く記事を配列に追加する。
+const insertItems = (entry, item_list) => {
+  for (let e of entry) {
+    if (e["app:control"][0]["app:draft"][0] == "yes") {
+      // 下書き記事をスキップする。
+      continue;
+    }
+
+    // はてな記事のJSONを生成。
+    const item = new HatenaItem(
+      moment(e.published.toString()).format("YYYY-MM-DD"),
+      e.title.toString(),
+      e.link[1].$.href
+    );
+    item_list.push(item);
   }
-  // Workspace直下で実行している前提です。
-  fs.writeFileSync('./client/assets/json/hatena.json', JSON.stringify(item_list));
-}
+};
+
+const main = async () => {
+  // 記事一覧を取得するURIは”https://blog.hatena.ne.jp/{はてなID}/{ブログID}/atom/entry”
+  let url =
+    "https://blog.hatena.ne.jp/korosuke613/korosuke613.hatenablog.com/atom/entry";
+
+  // 記事を格納する配列
+  let item_list = [];
+
+  // 下書き以外の記事が10件溜まるまで記事一覧を取得する
+  while (item_list.length < 10) {
+    const xml_data = await getHatenaData(url); // APIを叩いて記事一覧のXMLを取得する。
+    const { entry, next_url } = await extractItemsAndNextUri(xml_data); // 記事一覧と次の記事一覧のURIを抽出する。
+    insertItems(entry, item_list); // item_listに下書き以外の記事一覧を格納する。
+
+    url = next_url;
+  }
+
+  // 記事一覧をJSON形式で保存
+  fs.writeFileSync(
+    "./client/assets/json/hatena.json",
+    JSON.stringify(item_list)
+  );
+};
 
 main();
